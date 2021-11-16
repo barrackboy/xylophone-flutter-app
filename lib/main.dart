e4b3023d31fb80ad0fe75ef14d741443b5ee5b41
Collins <shout2me@gmail.com>
2021-11-17 01:48:45 +0200

    Functional 7 color xylophone without the intelligent Function

--- lib/main.dart
+++ lib/main.dart
@@ -4,61 +4,77 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+ 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note2.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note3.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note4.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note5.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note6.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.black,
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note7.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.black,
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
               ),
             ],
           ),
