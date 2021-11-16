50090e3994517c03ea956f67e5863e44a68c4e4f
Collins <shout2me@gmail.com>
2021-11-17 00:34:57 +0200

    click me tone with sound using audio cache

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(child: TextButton(onPressed: (){
+            final player = AudioCache();
+            player.play('note1.wav');
+
+          },child: Text('Click me'),)),
+        ),
+      ),
+    );
+  }
+}
